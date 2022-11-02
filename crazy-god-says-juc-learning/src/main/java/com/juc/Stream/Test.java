813326ff5f54224b3a2bf475b89773d19c9c68b0
shihan <3319824297@qq.com>
2022-11-02 23:11:40 +0800

    add：增加实例

--- crazy-god-says-juc-learning/src/main/java/com/juc/Stream/Test.java
+++ crazy-god-says-juc-learning/src/main/java/com/juc/Stream/Test.java
@@ -1,10 +1,7 @@
 package com.juc.Stream;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * 题目要求：一分钟内完成此题，只能用一行代码实现！
@@ -24,15 +21,24 @@ public class Test {
         User u5 = new User(6,"e",25);
         // 集合就是存储
         List<User> list = Arrays.asList(u1, u2, u3, u4, u5);
-        Map<String, User> userMap = list.stream().collect(Collectors.toMap(User::getName, user -> user,(k1, k2)->k1));
-        userMap.values().forEach(System.out::println);
-//        list.stream()
+//        Map<String, User> userMap = list.stream().collect(Collectors.toMap(User::getName, user -> user,(k1, k2)->k1));
+//        Map<Integer, List<User>> userMap = list.stream().collect(Collectors.groupingBy(User::getId));
+//        userMap.values().forEach(System.out::println);
+
+//        /**
+//         * 获取年纪最大和最小
+//         */
+//        Optional<User> maxAageUser = list.stream().max(Comparator.comparing(User::getAge));
+//        maxAageUser.ifPresent(user -> System.out.println("max age user"+user));
+
+//        System.out.println(userMap);
+//        List<User> list2 = list.stream().sorted(Comparator.comparing(User::getAge)).collect(Collectors.toList());
 //                .filter(u->{return u.getId()%2==0;})
 //                .filter(u->{return u.getAge()>23;})
 //                .map(u->{return u.getName().toUpperCase();})
 //                .sorted((uu1,uu2)->{return uu2.compareTo(uu1);})
 //                .limit(1)
-//                .forEach(System.out::println);
+//        System.out.println(list2);
 
 //        int sun = Stream.of(1,2,3).reduce(0,(a,b)->(a+b));
 //        System.out.println(sun);
@@ -43,5 +49,15 @@ public class Test {
 //        Stream<String> stream = Stream.of("A","B","C","D");
 //        boolean match = stream.allMatch(s -> s.contains("c"));
 //        System.out.println(match);
+
+
+//        HashMap<String,String> hashMap = new HashMap<>();
+//        hashMap.put("公众号","减田螺的小涵涵");
+//        hashMap.put("职业","程序员小涵涵");
+//        hashMap.put("昵称","爱做饭小涵涵");
+//        /**
+//         * forEach 遍历集合Map
+//         */
+//        hashMap.forEach((k, v)-> System.out.println(k+"\t"+v));
     }
 }
