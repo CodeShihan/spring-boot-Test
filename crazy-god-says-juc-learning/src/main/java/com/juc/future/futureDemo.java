1d0ed3c9cfd311eeac951a61645fdf1bcc18f242
shihan <3319824297@qq.com>
2022-11-06 20:52:41 +0800

    add：增加单例模式四种方法

---
+++ crazy-god-says-juc-learning/src/main/java/com/juc/future/futureDemo.java
@@ -0,0 +1,8 @@
+package com.juc.future;/**
+  * <Description> <br>
+  * @author shihan@qq.com<br>
+  * @version 1.0<br>
+  * @date 2022/11/06 <br>
+*/
+public class futureDemo {
+}
---
+++ crazy-god-says-juc-learning/src/main/java/com/juc/single/SingleTon.java
@@ -0,0 +1,26 @@
+package com.juc.single;
+
+/**
+ * <Description> 单例模式<br>
+ * @author shihan@qq.com<br>
+ * @version 1.0<br>
+ * @date 2022/11/06 <br>
+ */
+public class SingleTest {
+
+    public static void main(String[] args) {
+        /**
+         * 单例模式
+         * 什么是单列模式
+         * 答：单列模式就是一种创建型的设计模式，他保证了在一定范围或者作用域内只有一个类的实例，也就是创建对象实例
+         * 特点：1.创建型设计模式
+         *      2.保证范围内的一个实例
+         *      3.某个范围内呢只能有一个访问实例的结点
+         * 实现:
+         *     1. 懒汉模式
+         *     2. 饿汉模式
+         *     3. 静态模式
+         *     4. 枚举模式
+         */
+    }
+}
